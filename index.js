2af00927b02f08634b6e87379faabe9d298ccef6
Charlie Harris <ceharris92@gmail.com>
2016-04-03 13:19:12 +0100

    added airbnb liniting rules

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//TODO: use import;
-var express = require('express');
+// TODO: use import;
+const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -7,14 +7,13 @@ app.set('port', process.env.PORT || 3000);
 app.use((req, res) => {
   res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found')
-})
+  res.send('404 - Not Found');
+});
 
 app.use((req, res) => {
   res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error')
+  res.send('500 - Server Error');
 });
 
-app.listen(app.get('port'), () => console.log(`Express started on http://localhost:${app.get('port')}`));
-
+app.listen(app.get('port'));
