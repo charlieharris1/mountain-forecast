8a0ad484699f207efeab5fa91b469699310d35c3
Charlie Harris <ceharris92@gmail.com>
2016-04-10 18:01:10 +0100

    Added interactivity for detailed forecasts

--- src/ui/scripts/mountainForecast.js
+++ src/ui/scripts/mountainForecast.js
@@ -1,8 +1,20 @@
 var Area = React.createClass({
+  getInitialState: function() {
+    return { more: false };
+  },
+  handleClick: function(event) {
+    this.setState({ more: !this.state.more });
+  },
   render: function() {
+    var detailedForecast = this.state.more ? 'Detailed forecast' : '';
+    var plusMinus = this.state.more ? '-' : '+';
     return (
       <div className="area">
-        <h4 className="areaName"> {this.props.name}: {this.props.children.Risk} </h4>
+        <h4 className="areaName" onClick={this.handleClick}> {this.props.name}: {this.props.children.Risk} {plusMinus}
+          <p className="detailedAreaForecast">
+            {detailedForecast}
+          </p>
+        </h4>
       </div>
     );
   }
